06dc3e6abdd10534e3695c2e3f9d57ad84875a1a
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-13 11:19:27 -0400

    Initial commit

---
+++ src/Tax_Calculator.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1.14 Solution
+ *  Copyright 2021 Ronald Lemus
+ */
+
+
+public class Tax_Calculator {
+    public static void main(String [] arg){
+
+    }
+
+}
