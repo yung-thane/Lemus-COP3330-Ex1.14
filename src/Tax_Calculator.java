0dd83048d19b271a65017843aa96d85e2ee834fd
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-13 11:44:11 -0400

    Final Commit

--- src/Tax_Calculator.java
+++ src/Tax_Calculator.java
@@ -4,8 +4,37 @@
  */
 
 
+import java.util.Scanner;
+
 public class Tax_Calculator {
     public static void main(String [] arg){
+
+        System.out.print("What is the order amount? ");
+        Scanner orderInp = new Scanner(System.in);
+        double orderAmt = orderInp.nextDouble();
+
+        System.out.print("What is the state? ");
+        Scanner stateInp = new Scanner(System.in);
+        String state = stateInp.nextLine();
+
+        System.out.printf("The subtotal is $%.2f.\n", orderAmt);
+
+        double total = orderAmt;
+        double tax = 0;
+
+
+            if (state.equals("WI")){
+                double taxRate = .055;
+                tax = orderAmt * taxRate;
+                total = orderAmt + tax;
+            }
+        System.out.printf("The tax is $%.2f\n", tax);
+        System.out.printf("The total is $%.2f.", total);
+
+
+
+
+
 
     }
 
